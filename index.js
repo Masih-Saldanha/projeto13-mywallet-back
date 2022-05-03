73033018df18c3cd5d5b7b4a8bc4db572f73e755
Masih-Saldanha <masih.saldanha@gmail.com>
2022-05-03 12:14:44 -0300

    Base do servidor pronta, faltam rotas

---
+++ index.js
@@ -0,0 +1,19 @@
+import express, { json } from "express";
+import cors from "cors";
+import { MongoClient, ObjectId } from "mongodb";
+// import dayjs from "dayjs";
+// import joi from "joi";
+// import { stripHtml } from "string-strip-html";
+import dotenv from "dotenv";
+dotenv.config();
+
+const mongoClient = new MongoClient(process.env.MONGO_URI);
+let db;
+
+const app = express();
+app.use(cors());
+app.use(json());
+
+app.listen(process.env.PORTA, () => {
+    console.log(`Servidor ligado na porta ${process.env.PORTA}`)
+})
